38b82592b0747c782e2ac6cfa8f5a03ed4a4497e
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-05-10 17:52:45 +0000

    child class instantiation

---
+++ models/console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import cmd
+"""Class instance that inherits from the Cmd class"""
+
+
+class HBNBCommand(cmd.Cmd):
+    """Simple class to build an interpreter
+       prompt: custom entry point to the interpreter
+    """
+
+    prompt = '(hbnb) '
+    def do_EOF(self, arg):
+        """Command to handle the EOF"""
+        return True
+    
+    def do_quit(self, arg):
+        """Command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Command to not execute any empty line entered"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
