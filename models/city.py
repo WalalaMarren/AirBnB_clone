600776bf47be63d7420711e252ed5c1a30a76c05
Daniel Berthran <berthlearn@gmail.com>
2023-05-12 00:05:03 +0100

    proper imports and attribute setting

--- models/city.py
+++ models/city.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """class City that inherits from BaseModel"""
-from models.BaseModel import BaseModel
+from models.base_model import BaseModel
+from models.state import State
 
 
 class City(BaseModel):
     '''sets the name and state_id attributes to empty strings'''
-    
+
+    state_id = State.id
     name = ""
-    state_id = ""
