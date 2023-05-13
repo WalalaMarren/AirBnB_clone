4048835039582e84e237ced9f56d84d2eab120cb
Daniel Berthran <berthlearn@gmail.com>
2023-05-13 18:08:11 +0100

    pycodefix

--- marren/__init__.py
+++
@@ -1,2 +0,0 @@
-from marren import alx
-from marren.alx import verify_package
--- marren/alx.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-print("This is a folder")
-
-def verify_package():
-    print("This is a package")
