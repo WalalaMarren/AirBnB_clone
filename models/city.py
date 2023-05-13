f9badae4d411f09be787d816e1ca67fc3236186a
Daniel Berthran <berthlearn@gmail.com>
2023-05-13 18:49:53 +0100

    forced pycodestyle error

--- models/city.py
+++ models/city.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""class City that inherits from BaseModel"""
+"""class City that inherits from BaseModel
+
+Classes:
+    City : Defines a city with a name and id
+"""
 from models.base_model import BaseModel
 from models.state import State
 
@@ -8,3 +12,4 @@ class City(BaseModel):
     '''Defines a city with a name and id'''
     state_id = ""
     name = ""
+
