6904dea6f5f3f8f085d80bc1e921fc102b6c9fbf
Daniel Berthran <berthlearn@gmail.com>
2023-05-15 11:32:28 +0100

    proper inheritance

--- models/user.py
+++ models/user.py
@@ -10,8 +10,9 @@ from models.base_model import BaseModel
 class User(BaseModel):
     '''Defines the attributes of a user'''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Intialises a user's details like the name, password and email'''
+        super().__init__(self, *args, **kwargs)
         User.email = ""
         User.password = ""
         User.first_name = ""
