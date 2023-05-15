0b5d38275bb96fa62d9b189416a5595856f75e66
Daniel Berthran <berthlearn@gmail.com>
2023-05-15 12:32:20 +0100

    State attribute test

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''A test for state instances
-This tests the functions and functionality of the   module
+This tests the functions and functionality of the   state module
 '''
 import uuid as u
 import unittest
@@ -10,35 +10,26 @@ from datetime import datetime as d
 
 
 class TestBaseModel(unittest.TestCase):
-    '''A test for the city module'''
+    '''A test for the state module'''
 
     def setUp(self):
-        '''Initialises City instances'''
+        '''Initialises two State intances'''
         self.inst_id = str(u.uuid4())
         self.t1 = d.now()
         self.t2 = d.now()
         kwargs = {"id": self.inst_id,
                   "created_at": self.t1,
                   "updated_at": self.t2}
-        self.city_1 = State()
-        self.city_2 = State(**kwargs)
+        self.state_1 = State()
+        self.state_2 = State(**kwargs)
 
     def test_instance(self):
         '''Tests that a User instance was created'''
-        self.assertIsInstance(self.city_1, State)
-        self.assertIsInstance(self.city_2, State)
+        self.assertIsInstance(self.state_1, State)
+        self.assertIsInstance(self.state_2, State)
 
-    def test_save(self):
-        '''Tests that updated_at is updated when the
-        instance is saved'''
-        c1_ut1 = self.city_1.updated_at
+    def test_cls_attributes(self):
+        '''Tests that the class attribute is correct'''
+        self.State = State
 
-        c1_id = self.city_1.id
-        c1_key = "State." + c1_id
-
-        self.city_1.save()
-        c1_ut2 = self.city_1.updated_at
-        all_objs = storage.all()
-
-        self.assertNotEqual(c1_ut1, c1_ut2)
-        self.assertIn(c1_key, all_objs.keys())
+        self.assertEqual(self.State.name, "")
