b8f9d33d1912a3fc53bc9d9e725607275d45c036
Daniel Berthran <berthlearn@gmail.com>
2023-05-15 11:20:50 +0100

    public class attributes

--- models/user.py
+++ models/user.py
@@ -9,7 +9,10 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     '''Defines the attributes of a user'''
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+
+    def __init__(self):
+        '''Intialises a user's details like the name, password and email'''
+        User.email = ""
+        User.password = ""
+        User.first_name = ""
+        User.last_name = ""
